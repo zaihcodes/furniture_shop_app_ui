0b79d6a127b9ee55cc07e062939e3623828fbea3
ZI-DreamWork <zaihwork@gmail.com>
2024-06-16 13:48:43 +0100

    changed the welcomescreen button name

---
+++ lib/core/utils/widgets/welcome_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class WelcomeButtom extends StatelessWidget {
+  const WelcomeButtom({
+    super.key,
+    required this.text,
+    this.bgColor,
+    this.textColor = Colors.white,
+    this.fontSize = 18,
+    this.textStyle,
+    this.onTap,
+  });
+
+  final String text;
+  final Color? bgColor;
+  final Color textColor;
+  final double? fontSize;
+  final TextStyle? textStyle;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 54,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        decoration: BoxDecoration(
+          color: theme.colorScheme.primary,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          text,
+          style: (textStyle ?? theme.textTheme.bodyMedium)!.copyWith(
+            fontSize: fontSize ?? 18,
+            color: textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
