8c001b72b264a9adfee6c718d7ee2c6993384c4f
ZI-DreamWork <zaihwork@gmail.com>
2024-06-29 20:58:53 +0100

    added product model and product data with some data

---
+++ lib/core/data/product_data.dart
@@ -0,0 +1,51 @@
+import '../models/product_model.dart';
+
+class ProductData {
+  static List<ProductModel> data = [
+    const ProductModel(
+      name: 'Minimal Lamp',
+      price: 12.00,
+      rating: 4.5,
+      review: 50,
+      imagePath: 'assets/images/black_simple_lamp.png',
+      description:
+          'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
+    ),
+    const ProductModel(
+      name: 'Minimal Stand',
+      price: 25.00,
+      rating: 4.5,
+      review: 50,
+      imagePath: 'assets/images/minimal_stand.png',
+      description:
+          'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
+    ),
+    const ProductModel(
+      name: 'Coffee Chair',
+      price: 20.00,
+      rating: 4.5,
+      review: 50,
+      imagePath: 'assets/images/coffee_chair.png',
+      description:
+          'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
+    ),
+    const ProductModel(
+      name: 'Simple Desk',
+      price: 50.00,
+      rating: 4.5,
+      review: 50,
+      imagePath: 'assets/images/simple_desk.png',
+      description:
+          'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
+    ),
+    const ProductModel(
+      name: 'Coffee Table',
+      price: 50.00,
+      rating: 4.5,
+      review: 50,
+      imagePath: 'assets/images/coffee_table.png',
+      description:
+          'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
+    ),
+  ];
+}
---
+++ lib/core/models/product_model.dart
@@ -0,0 +1,17 @@
+class ProductModel {
+  final String name;
+  final double price;
+  final double rating;
+  final int review;
+  final String description;
+  final String imagePath;
+
+  const ProductModel({
+    required this.name,
+    required this.price,
+    required this.rating,
+    required this.review,
+    required this.description,
+    required this.imagePath,
+  });
+}
